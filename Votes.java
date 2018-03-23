ea9df0c025e8913f6b14dd2bf23357eaa9d099ea
Brandon Lim <brandon.lim.16@ucl.ac.uk>
2018-03-23 15:46:02 +0000

    test

--- Votes.java
+++ Votes.java
@@ -1,6 +1,6 @@
 //
 // The purpose of this class is to create an object type Votes which we can use to store voting information.
-// As of now Votes is a separate class as we were fiddling with ways to store voting / voter information 
+// As of now Votes is a separate class as we were fiddling with ways to store voting / voter information
 // thus we will work on improving this in the next deliverable
 
 public class Votes {
@@ -14,6 +14,4 @@ public class Votes {
         return listOfScores;
     }
 
-//
-    //
 }
